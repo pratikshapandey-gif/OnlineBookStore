31f94118b723d02d0416980951042f4fd9b2934b
Pratiksha Pandey <pratikshapandeyimec2023@gmail.com>
2025-07-28 11:36:06 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,52 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private double price;
+    private int stock;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+  
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
